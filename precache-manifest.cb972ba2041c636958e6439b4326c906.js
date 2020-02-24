cbd87ea2d57e115712395937fde80f4171ea0ced
Ameet <kameetkumar@gmail.com>
2020-02-24 17:09:46 +0530

    Build folder files added

---
+++ precache-manifest.cb972ba2041c636958e6439b4326c906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7ba555e05b476f0b5fb451d1abf799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13cc174c40ea460d23b0",
+    "url": "/static/css/main.ee04d31d.chunk.css"
+  },
+  {
+    "revision": "e5d219a6ea2d6025a82c",
+    "url": "/static/js/2.3465ec0f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3465ec0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cc174c40ea460d23b0",
+    "url": "/static/js/main.2482d9f8.chunk.js"
+  },
+  {
+    "revision": "c84ae7b26dd1220ffeab",
+    "url": "/static/js/runtime-main.09b16d19.js"
+  }
+]);
